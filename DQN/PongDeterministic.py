336a2d265bf5d4230c989602d6ff925b2d3e0533
piotrek <piotrpiekos@gmail.com>
2019-06-25 20:47:21 +0200

    created PongDeterministic version of agent

---
+++ DQN/PongDeterministic.py
@@ -0,0 +1,51 @@
+import gym
+import tensorflow as tf
+
+from agents.DQN_Agent import DQN_Agent
+from utils.StateProcessors import DQN_StateProcessor
+
+
+NUM_EPISODES = 100000
+EP_LENGTH = 100000
+
+env = gym.make('PongDeterministic-v4')
+
+ACTIONS_REPEATED = 4
+
+agent = DQN_Agent(env)
+state_processor = DQN_StateProcessor()
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+with tf.Session(config=config) as sess:
+    sess.run(tf.global_variables_initializer())
+    agent.set_session(sess)
+
+    for i_episode in range(NUM_EPISODES):
+
+        observation = env.reset()
+        state_processor.clear_episode()
+        agent.next_episode()
+        observation = state_processor.process_state(observation)
+
+        done = False
+
+        i_frame = 0
+        while not done:
+            if i_episode % 3 == 0:
+                env.render()
+
+            if i_frame % ACTIONS_REPEATED == 0:
+                action = agent.take_action(observation)
+            observation_new, reward, done, info = env.step(action)
+
+            observation_new = state_processor.process_state(observation_new)
+
+            if i_frame % ACTIONS_REPEATED == 0:
+                agent.observe(observation, action, reward, observation_new, done)
+
+            observation = observation_new
+
+            if i_frame % ACTIONS_REPEATED == 0:
+                agent.step_train()
